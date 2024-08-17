788dad41c16a3215566109b67bb38c5967d61d0c
Daniel Porteous <daniel@dport.me>
2024-08-17 11:23:08 +0100

    Set initial color mode of frontend to "system"

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
+    <ColorModeScript initialColorMode="system" />
     <App />
   </React.StrictMode>,
 );
